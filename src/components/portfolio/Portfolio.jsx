404df846c50328338bad5f3771e690a5e4c44ea6
Darth-InVader15 <piyush.singh1315@gmail.com>
2024-03-31 00:46:42 +0530

    added projects details

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,73 +6,78 @@ const items = [
   {
     id: 1,
     title: "Full Stack Projects",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "/mern.png",
     desc: "",
     url: "",
   },{
     id: 2,
     title: "E-Commerce Website",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "/Ecom.png",
     desc: "A modern and seamless Shopping platform with modern and materialistic UI. It supports both User and Seller signups and logins. It uses JWT for authentication, ExpressJS for Server side development, ReactJS and MUI for frontend and MongoDB as database.",
+    url: "https://github.com/ShouryaTyagi042/EcommerceProject",
   },{
     id: 3,
     title: "AttenDed - Attendance Management WebApp",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "/Attended.png",
     desc: "AttenDed is a webapp I designed for tracking my college's attendance. It uses Firebase for authentication, ExpressJS for Server side development, ReactJS for frontend and MongoDB as database.",
-    url: "",
+    url: "https://github.com/Darth-InVader15/AttenDED",
   },
   {
     id: 4,
     title: "EMR's Official Website",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "/emr.png",
     desc: "The official website of the NITKKR's Embedded and Robotics Club.",
-    url: "",
+    url: "https://github.com/yujit2003/WebD-2.0-1",
   },
-  {
-    id: 5,
-    title: "ProfReview - a website to rate your professors",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Worked on the backend of the website. Users can rate their professor on basis of difficulty of coursework, homeworks, leniency in assessment,etc. It uses Firebase for authentication, ExpressJS for Server side development, and MongoDB as database.",
-  },
+  // {
+  //   id: 5,
+  //   title: "ProfReview - a website to rate your professors",
+  //   img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  //   desc: "Worked on the backend of the website. Users can rate their professor on basis of difficulty of coursework, homeworks, leniency in assessment,etc. It uses Firebase for authentication, ExpressJS for Server side development, and MongoDB as database.",
+  // },
 ];
 const Android = [
   {
-    id: 3,
+    id: 6,
     title: "Android Projects",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "/android.png",
     desc: "",
   },
   {
-    id: 4,
+    id: 7,
     title: "Ambisonic Music App (8D Audio)",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "/x.png",
+    desc: "This app takes any normal audio and pans its sound in 8 directions to give a 3D effect. It uses the Ambisonic Audio format to achieve this. The app uses the ExoPlayer library to play the audio.",
+    url: "https://github.com/Darth-InVader15/8D-Audio",
   },
   {
-    id: 4,
+    id: 8,
     title: "Quotes App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "/quote2.png",
+    desc: "A simple app that displays a random quote every five seconds. The app uses the Retrofit library to fetch the quotes from an API.",
+    url: "https://github.com/Darth-InVader15/Quotes.App",
   },
 ];
 const Embedded = [
   {
-    id: 4,
+    id: 9,
     title: "Embedded Systems Projects",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "/uno.jpg",
+    desc: "",
   },
   {
-    id: 4,
+    id: 10,
     title: "2D Maze Solver Bot",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "/bot.jpg",
+    desc: "This bot uses the 3 PIR sensors on its front and uses DFS algorithm to traverse through any 2D maze. For this repo, an Arduino UNO was used as the microcontroller.",
+    url: "https://github.com/Darth-InVader15/Labyrinth-Solver-Bot",
   },
   {
-    id: 4,
+    id: 11,
     title: "WiFi controlled Bot",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "/wifibot.jpg",
+    desc: "This bot uses ESP8266 to connect to the internet and can be controlled using a web interface. This bot can further be furnished with a camera to make it a surveillance bot.",
+    url: "https://github.com/Darth-InVader15/WiFi-Controlled-Bot",
   },
 ];
 
@@ -85,6 +90,10 @@ const Single = ({ item }) => {
 
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
+  const handleClick = () => {
+    window.open(item.url, '_blank');
+  };
+
   return (
     <section >
       <div className="container">
@@ -95,7 +104,8 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button href="{item.url}">See on Github</button>
+
+            {item.url && <button onClick={handleClick}>See on Github</button>}
           </motion.div>
         </div>
       </div>
